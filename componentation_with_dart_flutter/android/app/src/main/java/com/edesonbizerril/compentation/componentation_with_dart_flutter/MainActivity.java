146dd13b4991302888da37b99eec3aab592bafe1
Edeson Bizerril <edeson.alves@hotmail.com>
2023-01-04 16:39:42 -0300

    bottom use - super commit

---
+++ componentation_with_dart_flutter/android/app/src/main/java/com/edesonbizerril/compentation/componentation_with_dart_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.edesonbizerril.compentation.componentation_with_dart_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
